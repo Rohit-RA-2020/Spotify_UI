79e7b61b1fecf36972f9485c9bb4ade4553d0c55
Rohit ranjan <ranjanrohit812@gmail.com>
2021-01-15 13:58:39 +0530

    Added home_page Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'home_page.dart';
 
 void main() => runApp(MyApp());
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        canvasColor: Color(0xFF191919),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        fontFamily: GoogleFonts.montserrat().fontFamily,
+      ),
+      home: HomePage(),
     );
   }
 }
