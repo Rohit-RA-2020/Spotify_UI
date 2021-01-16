b43d33ee2f812c3b21c6b06a28517db6c1010714
Rohit ranjan <ranjanrohit812@gmail.com>
2021-01-16 20:16:34 +0530

    Added bottom navigation bar

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -4,6 +4,59 @@ import 'package:google_fonts/google_fonts.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Color(0xFF191919),
+      appBar: AppBar(
+        backgroundColor: Color(0xFF191919),
+        elevation: 0,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 8.0),
+            child: Icon(Icons.settings_outlined),
+          )
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.transparent,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white.withOpacity(0.8),
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_filled),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: "Search",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.library_books_outlined),
+            label: "Library",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shop),
+            label: "Premium",
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 16.0),
+              child: Text(
+                "Good Afternoon",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22,
+                  color: Colors.white,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
