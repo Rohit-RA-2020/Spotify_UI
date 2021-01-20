3cbb8d98e32f123f8b701067eb803c9279e0f177
Rohit ranjan <ranjanrohit812@gmail.com>
2021-01-20 13:18:02 +0530

    Added few cards

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -53,6 +54,93 @@ class HomePage extends StatelessWidget {
                   color: Colors.white,
                 ),
               ),
+            ),
+            const SizedBox(height: 24.0),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  AlbumMini(
+                    image: 'assets/kendrick.jpg',
+                    text: "Kendrick\nLamar",
+                  ),
+                  AlbumMini(
+                    image: 'assets/damn.jpg',
+                    text: "Damn",
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 8.0),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  AlbumMini(
+                    image: 'assets/jcole.jpg',
+                    text: 'J. Cole',
+                  ),
+                  AlbumMini(
+                    image: 'assets/lofi.jpeg',
+                    text: 'Lo-fi Beats',
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(height: 24.0),
+            Padding(
+              padding: const EdgeInsets.only(left: 16.0),
+              child: Text(
+                "Recently Played",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22,
+                  color: Colors.white
+                ),
+              ),
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
+class AlbumMini extends StatelessWidget {
+  final String image;
+  final String text;
+
+  const AlbumMini({Key key, this.image, this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 175,
+      height: 60,
+      decoration: BoxDecoration(
+        color: Color(0xFF313132),
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(8.0),
+        child: Row(
+          children: [
+            Container(
+              width: 60,
+              height: 60,
+              child: Image.asset(
+                image,
+                filterQuality: FilterQuality.high,
+              ),
+            ),
+            const SizedBox(width: 8.0),
+            Text(
+              text,
+              style: TextStyle(
+                color: Colors.white,
+              ),
             )
           ],
         ),
@@ -60,3 +148,4 @@ class HomePage extends StatelessWidget {
     );
   }
 }
+
